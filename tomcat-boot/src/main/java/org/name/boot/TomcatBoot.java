7b60fb84df98d7d7f7ad6f92e8af5ed4a9728f0a
whoopsjohnnie <jgrundback@gmail.com>
2019-07-22 11:29:46 -0700

    Initial commit of Tomcat Spring Boot project

---
+++ tomcat-boot/src/main/java/org/name/boot/TomcatBoot.java
@@ -0,0 +1,13 @@
+package org.name.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatBoot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomcatBoot.class, args);
+	}
+
+}
