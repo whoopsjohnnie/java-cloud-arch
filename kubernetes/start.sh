65fa372fab383e4202e2e31bc62adf3fdd15a65f
whoopsjohnnie <jgrundback@gmail.com>
2019-07-25 12:31:40 -0700

    Ingress work for k8s

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -6,6 +6,18 @@ kubectl apply -f ./namespace.yaml
 
 kubectl apply -f ./authentication-deployment.yaml
 kubectl apply -f ./authentication-service.yaml
+# kubectl apply -f ./authentication-ingress.yaml
 
 kubectl apply -f ./tomcat-deployment.yaml
 kubectl apply -f ./tomcat-service.yaml
+# kubectl apply -f ./tomcat-ingress.yaml
+
+
+# 
+# kubectl create -f traefik-service-acc.yaml
+# kubectl create -f traefik-cr.yaml
+# kubectl create -f traefik-crb.yaml
+# kubectl create -f traefik-deployment.yaml
+# kubectl create -f traefik-svc.yaml
+# kubectl create -f traefik-webui-svc.yaml
+# kubectl create -f traefik-ingress.yaml
--- kubernetes/stop.sh
+++ kubernetes/stop.sh
@@ -2,9 +2,21 @@
 
 set -x
 
+#
+# kubectl delete -f traefik-ingress.yaml
+# kubectl delete -f traefik-webui-svc.yaml
+# kubectl delete -f traefik-svc.yaml
+# kubectl delete -f traefik-deployment.yaml
+# kubectl delete -f traefik-crb.yaml
+# kubectl delete -f traefik-cr.yaml
+# kubectl delete -f traefik-service-acc.yaml
+
+
+# kubectl delete -f ./tomcat-ingress.yaml
 kubectl delete -f ./tomcat-service.yaml
 kubectl delete -f ./tomcat-deployment.yaml
 
+# kubectl delete -f ./authentication-ingress.yaml
 kubectl delete -f ./authentication-service.yaml
 kubectl delete -f ./authentication-deployment.yaml
 
